287318b3ce431fcab8edb0a5da6f6c8b334bfdab
Manminder <ManminderSingh@edu.sait.ca>
2024-11-20 17:20:24 -0700

    thanks arron

--- app/page.js
+++ app/page.js
@@ -2,21 +2,7 @@
 
 const AppLayout = () => {
   return (
-    <div className="app-layout">
-      <nav className="bg-gray-800 text-white p-4">
-        <ul className="flex space-x-4">
-          <li className="hover:text-blue-600 cursor-pointer transition duration-300">Home</li>
-          <li className="hover:text-blue-600 cursor-pointer transition duration-300">Browse</li>
-          <li className="hover:text-blue-600 cursor-pointer transition duration-300">Library</li>
-        </ul>
-      </nav>
-      <main className="flex-grow p-4">
-        {/* Main content goes here */}
-      </main>
-      <footer className="bg-gray-800 text-white p-4 text-center">
-        <p>© 2023 SoundScape</p>
-      </footer>
-    </div>
+   <div>hello</div>
   );
 };
 
